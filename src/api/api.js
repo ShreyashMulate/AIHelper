cb3814d37dcfa3886c06e5cd6b5f72fd4c559da8
Shreyash Mulate <shreyash@bitcanny.com>
2025-08-29 19:50:09 +0530

    changes

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://c3de815a8b1f.ngrok-free.app/api"
+  baseURL: `${process.env.VITE_API_BASE_URL}/api`
 });
 
 export const improveText = (text, field, model) => API.post("/improve", { text, field, model });
 export const fixGrammar = (text, model) => API.post("/fix", { text, model });
-  
